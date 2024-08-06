e9285aaae3aa69074b75e1149944f37f307d5e3b
OfentseXI <dofentse69@gmail.com>
2024-08-06 11:33:58 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup>
 import Header from './components/Header.vue';
+import Home from './pages/Home.vue'
 </script>
 
 
 <template>
   <Header />
   <main>
-    
+    <Home />
   </main>
 </template>
 
