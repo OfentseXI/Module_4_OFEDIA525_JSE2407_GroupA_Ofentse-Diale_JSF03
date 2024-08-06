1f15d9d38284b098b65b695ebae462a6d1f6ecaf
OfentseXI <dofentse69@gmail.com>
2024-08-06 11:33:55 +0200

    Update Home.vue

---
+++ src/pages/Home.vue
@@ -0,0 +1,13 @@
+<script>
+    import FilterAndSort from "../components/FilterAndSort.vue";
+    import ProductsList from "../components/ProductsList.vue";
+</script>.
+
+<template>
+<main class="bg-[#81ccdd]">
+    <div>
+        <FilterSort />
+    </div>
+    <ProductsList />
+</main>
+</template>
