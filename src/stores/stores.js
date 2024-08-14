c5113730d33160b94cdccf351478b83e78d7c790
OfentseXI <dofentse69@gmail.com>
2024-08-14 21:32:13 +0200

    Updated ProductGrid.vue component and added shopping store with API call and cart management functionality

--- src/stores/stores.js
+++ src/stores/stores.js
@@ -1,3 +1,92 @@
 import { defineStore } from 'pinia'
 import Swal from 'sweetalert2'
+import axios from 'axios'
 
+export const useShoppingStore = defineStore('shopping', {
+  state: () => {
+    return {
+      products: [],
+      cartItems: []
+    }
+  },
+  getters: {
+    countCartItems() {
+      return this.cartItems.length
+    },
+    getCartItems() {
+      return this.cartItems
+    }
+  },
+  actions: {
+    async fetchProducts() {
+      try {
+        const response = await axios.get('https://your-api-url.com/products') // replace with your API URL
+        this.products = response.data
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    addToCart(item) {
+      let index = this.cartItems.findIndex(product => product.id === item.id)
+      if (index !== -1) {
+        this.cartItems[index].quantity += 1
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'Your item has been updated',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      } else {
+        item.quantity = 1
+        this.cartItems.push(item)
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'Your item has been saved',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      }
+    },
+    incrementQ(item) {
+      let index = this.cartItems.findIndex(product => product.id === item.id)
+      if (index !== -1) {
+        this.cartItems[index].quantity += 1
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'Your item has been updated',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      }
+    },
+    decrementQ(item) {
+      let index = this.cartItems.findIndex(product => product.id === item.id)
+      if (index !== -1) {
+        this.cartItems[index].quantity -= 1
+        if (this.cartItems[index].quantity === 0) {
+          this.cartItems = this.cartItems.filter(product => product.id !== item.id)
+        }
+        Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: 'Your item has been updated',
+          showConfirmButton: false,
+          timer: 1500
+        })
+      }
+    },
+    removeFromCart(item) {
+      this.cartItems = this.cartItems.filter(product => product.id !== item.id)
+      Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Your item has been removed',
+        showConfirmButton: false,
+        timer: 1500
+      })
+    }
+  }
+})
