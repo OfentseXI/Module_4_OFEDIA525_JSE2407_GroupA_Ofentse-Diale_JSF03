ecb1751a9a95370ae2cfa949a13bf124ee7708fd
OfentseXI <dofentse69@gmail.com>
2024-08-15 14:01:10 +0200

    bootstap and bootstrap icons installation

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue';
 import routing from './router';
 import { createPinia } from 'pinia'; 
 import 'sweetalert2/dist/sweetalert2.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const pinia = createPinia();
 const app = createApp(App);
