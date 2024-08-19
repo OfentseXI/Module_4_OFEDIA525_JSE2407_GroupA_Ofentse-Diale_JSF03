314fe83893260557a951489276a4eee6ae240f46
OfentseXI <dofentse69@gmail.com>
2024-08-19 22:44:38 +0200

    Removed Bootstrap CSS and JS imports from main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import App from './App.vue';
 import routing from './router';
 import { createPinia } from 'pinia'; 
 import 'sweetalert2/dist/sweetalert2.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const pinia = createPinia();
