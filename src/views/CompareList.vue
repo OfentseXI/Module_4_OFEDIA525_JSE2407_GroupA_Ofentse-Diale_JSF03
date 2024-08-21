1d67899acf611f30fef24596b22a022817d3032f
OfentseXI <dofentse69@gmail.com>
2024-08-22 00:47:14 +0200

    Updated CompareList.vue: added index to v-for loop, changed button click handler, added cart functionality, and implemented Swal notifications

--- src/views/CompareList.vue
+++ src/views/CompareList.vue
@@ -14,7 +14,7 @@
             </tr>
           </thead>
           <tbody>
-            <tr v-for="product in compareList" :key="product.id">
+            <tr v-for="(product, index) in compareList" :key="product.id">
                 <td class="py-4 px-6 border-b border-gray-200">
                   <img :src="product.image" :alt="product.title" class="w-24 h-24 object-contain rounded" />
                 </td>
@@ -37,12 +37,12 @@
                         Remove
                     </button>
                     <button 
-                        @click="removeFromCompareList(index)"
+                        @click="addToCart(product)"
                         class="bg-blue-900 hover:bg-blue-300 text-white font-bold py-1 px-2 rounded my-4 mx-2">
                         Add to cart
                     </button>
                 </td>
-              </tr>
+            </tr>
           </tbody>
         </table>
         <button 
@@ -52,41 +52,83 @@
             Clear Compare List
         </button>
         <p v-else class="text-gray-500 my-2">No products to compare. Please add products to the comparison list.</p>
-      </div>
-      <button></button>
-</template>  
+    </div>
+</template>
 
 <script>
 import { ref, onMounted } from 'vue';
+import Swal from 'sweetalert2';
 
 export default {
   name: 'CompareList',
   setup() {
     const compareList = ref([]);
+    const cart = ref([]);
 
     onMounted(() => {
       const storedCompareList = localStorage.getItem('compareList');
       if (storedCompareList) {
         compareList.value = JSON.parse(storedCompareList);
       }
+
+      const storedCart = localStorage.getItem('cart');
+      if (storedCart) {
+        cart.value = JSON.parse(storedCart);
+      }
     });
 
     const removeFromCompareList = (index) => {
       compareList.value.splice(index, 1);
       localStorage.setItem('compareList', JSON.stringify(compareList.value));
+      
+      Swal.fire({
+        icon: 'success',
+        title: 'Removed!',
+        text: 'The product has been removed from the comparison list.',
+        timer: 1500,
+        showConfirmButton: false,
+      });
     };
 
     const clearCompareList = () => {
       compareList.value = [];
       localStorage.removeItem('compareList');
+
+      Swal.fire({
+        icon: 'success',
+        title: 'Cleared!',
+        text: 'The comparison list has been cleared.',
+        timer: 1500,
+        showConfirmButton: false,
+      });
     };
 
+    const addToCart = (product) => {
+      const existingProduct = cart.value.find(item => item.id === product.id);
+
+      if (existingProduct) {
+        existingProduct.quantity += 1;
+      } else {
+        cart.value.push({ ...product, quantity: 1 });
+      }
+
+      localStorage.setItem('cart', JSON.stringify(cart.value));
+
+      Swal.fire({
+        icon: 'success',
+        title: 'Added to Cart!',
+        text: 'The product has been added to your cart.',
+        timer: 1500,
+        showConfirmButton: false,
+      });
+    };
 
     return {
       compareList,
       removeFromCompareList,
-      clearCompareList
+      clearCompareList,
+      addToCart,
     };
-  }
+  },
 };
 </script>
