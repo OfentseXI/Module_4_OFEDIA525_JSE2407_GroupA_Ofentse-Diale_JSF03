4af5dd67f93e2ed8903e4091019d0d62e550b072
OfentseXI <dofentse69@gmail.com>
2024-08-22 01:50:29 +0200

    Added Vuex to project, updated router to use Vuex store for authentication, and modified Cart.vue to display login message when not authenticated

--- src/router/index.js
+++ src/router/index.js
@@ -5,28 +5,31 @@ import ProductDetail from '../views/ProductDetails.vue';
 import Wishlist from '../views/Wishlist.vue';
 import Cart from '../views/Cart.vue';
 import CompareList from '../views/CompareList.vue';
+import store from '../stores/loginStore'
+
 
 const routes = [
   { path: '/', name:'home', component: Home },
   { path: '/login', name:'login', component: Login },
   { path: '/product/:id', name:'product', component: ProductDetail, props: true, meta: { requiresAuth: true } },
-  { path: '/wishlist', name:'wishlist', component: Wishlist },
-  { path: '/cart', name:'cart', component: Cart },
-  { path: '/compare', name:'compare', component: CompareList}
+  { path: '/wishlist', name:'wishlist', component: Wishlist, meta: { requiresAuth: true }},
+  { path: '/cart', name:'cart', component: Cart, meta: { requiresAuth: true }},
+  { path: '/compare', name:'compare', component: CompareList, meta: { requiresAuth: true }}
 ];
 
 const router = createRouter({
-  history: createWebHistory('/'),  // Updated to use a static base URL
+  history: createWebHistory(import.meta.env.BASE_URL),  // Updated to use a static base URL
   routes
 });
 
 // Global Navigation Guard
 router.beforeEach((to, from, next) => {
-  const loggedIn = localStorage.getItem('jwt');
-
-  if (to.matched.some(record => record.meta.requiresAuth) && !loggedIn) {
-    localStorage.setItem('redirectPath', to.fullPath);
-    next('/login');
+  const isLoggedIn = store.getters.isLoggedIn;
+  if (to.meta.requiresAuth && !isLoggedIn) {
+    next({
+      path: "/login",
+      query: { redirect: to.fullPath }, 
+    });
   } else {
     next();
   }
--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -55,6 +55,18 @@
     <p v-else class="text-gray-500 my-2 text-center">Your cart is empty.</p>
   </div>
 </div>
+<div v-else class="grid m-10 space-y-5">
+  <div
+    class="flex flex-col items-center bg-white border-2 border-gray-500 p-4"
+  >
+    <p class="text-lg line-clamp-2 font-extrabold leading-snug text-slate-600">
+      Please log in to view items in cart
+    </p>
+    <button class="flex rounded justify-center mt-3 bg-[#381257] px-3 py-2 text-sm font-medium text-white hover:bg-violet-500 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300">
+      <a href="/login">Login</a>
+    </button>
+  </div>
+</div>
 </template>
 
 <script setup>
