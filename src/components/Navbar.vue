3fe352bafd46c9ea9b90eaaa5525fdd774ce4a07
OfentseXI <dofentse69@gmail.com>
2024-08-21 23:32:28 +0200

    Updated CSS classes for navbar links

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,8 +7,8 @@
            </div> 
         </a>  
         <div class="hidden md:flex">
-          <a href="/compare">compare</a>
-          <a href="/wishlist" class="mr-4 hover:underline">wishlist</a>
+          <a href="/compare" class="mr-4 hover:underline">compare</a>
+          <a href="/wishlist" >wishlist</a>
           <a href="/cart" class="mr-4 hover:underline">cart</a>
           <a v-if="!isAuthenticated" href="/login" class="mr-4 hover:underline">Login</a>
           <a v-else @click="logout" class="mr-4 hover:underline cursor-pointer">Logout</a>
